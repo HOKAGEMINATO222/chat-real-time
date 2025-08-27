ac03f5c029f4222b2d84a3116047f72e4fffd866
HOKAGEMINATO222 <soicodoc2501@gmail.com>
2025-08-27 23:38:03 +0700

    inital commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { ENV } from './config/env.js';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World! 11sw');
+});
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on http://localhost:${ENV.PORT}`);
+}); 
