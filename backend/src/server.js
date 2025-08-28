c82034e01164c91e84e2e9457a5728d44bd0cf5d
HOKAGEMINATO222 <soicodoc2501@gmail.com>
2025-08-28 13:55:36 +0700

    db connected & env.js completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
 const app = express();
 dotenv.config();
@@ -12,4 +13,5 @@ app.get('/', (req, res) => {
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on http://localhost:${ENV.PORT}`);
+  connectDB();
 }); 
